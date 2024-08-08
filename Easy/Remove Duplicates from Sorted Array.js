22fa08386956a83fa9b2b69014aab0bc554c2cd3
MohamedMohamed <m01126165016@icloud.com>
2024-08-08 22:46:21 +0300

    26.Remove Duplicates from Sorted Array

--- Easy/Remove Duplicates from Sorted Array.js
+++ Easy/Remove Duplicates from Sorted Array.js
@@ -12,7 +12,7 @@ var removeDuplicates = function (nums) {
 };
 
 /**
- * Remove Duplicates from Sorted Array
+ * 26.Remove Duplicates from Sorted Array
  * https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
 
 Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
